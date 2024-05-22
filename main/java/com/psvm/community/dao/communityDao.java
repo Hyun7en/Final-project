9b4e2dbd2b9812682563df216018d67903c9b9a5
lhy9496 <dlghdus1949@gmail.com>
2024-05-22 18:28:24 +0900

    [이호연] 커뮤니티 백엔드 뼈대2

--- main/java/com/psvm/community/dao/communityDao.java
+++ main/java/com/psvm/community/dao/communityDao.java
@@ -1,8 +1,55 @@
 package com.psvm.community.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.psvm.attachment.CommunityAttachment;
+import com.psvm.commons.vo.PageInfo;
+import com.psvm.community.vo.Community;
+import com.psvm.community.vo.Reply;
 
 @Repository
 public class CommunityDao {
+	public int selectListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("communityMapper.selectListCount");
+	}
 	
+	public ArrayList<Community> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		return (ArrayList)sqlSession.selectList("communityMapper.selectList", null, rowBounds);
+	}
+	
+	public int increaseCount(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.update("communityMapper.increaseCount", boardNo);
+	}
+	
+	public Community selectBoard(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.selectOne("communityMapper.selectBoard", boardNo);
+	}
+	
+	public ArrayList<Reply> selectReply(SqlSessionTemplate sqlSession, int boardNo){
+		return (ArrayList)sqlSession.selectList("communityMapper.selectReply", boardNo);
+	}
+	
+	public int insertBoard(SqlSessionTemplate sqlSession, Community c, CommunityAttachment ca) {
+		return sqlSession.insert("communityMapper.insertBoard", c);
+	}
+	
+	public int updateBoard(SqlSessionTemplate sqlSession, Community c, CommunityAttachment ca) {
+		return sqlSession.update("communityMapper.updateBoard", c);
+	}
+	
+	public int insertReply(SqlSessionTemplate sqlSession, Reply r) {
+		return sqlSession.insert("communityMapper.insertReply", r);
+	}
+	
+	public ArrayList<Community> selectTopBoardList(SqlSessionTemplate sqlSession){
+		return (ArrayList)sqlSession.selectList("communityMapper.selectTopBoardList");
+	}
 }
+
