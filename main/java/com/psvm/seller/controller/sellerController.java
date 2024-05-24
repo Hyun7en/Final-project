b75f426b69040fe209b5173f85a1a91eebadc15a
Hyun7en <rhqudgus99@gmail.com>
2024-05-24 12:36:53 +0900

    [고병현] seller

--- main/java/com/psvm/seller/controller/sellerController.java
+++ main/java/com/psvm/seller/controller/sellerController.java
@@ -1,12 +1,29 @@
 package com.psvm.seller.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.psvm.seller.service.SellerService;
+import com.psvm.seller.vo.SellerInfo;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class SellerController {
+	
+	
+	private SellerService sellerService;
 
-//	public select
+	public String selectList(Model model) {
+		
+		ArrayList<SellerInfo> list = sellerService.selectList();
+		
+		model.addAttribute(list);
+		
+		
+		
+	}
 }
