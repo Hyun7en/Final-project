6c86245e5d9d899b04dbf2eb7ad0bb14fffc9ddc
DodleD <kdh030777@gmail.com>
2024-06-04 17:45:49 +0900

    [김도헌] 큐레이션

--- main/webapp/resources/js/curationJS/curation.js
+++ main/webapp/resources/js/curationJS/curation.js
@@ -1,7 +1,5 @@
 let path;
-
-let count;
-
+let count =0 ;
 function fishInfoInit(contextPath){
     path = contextPath;
 }
@@ -9,8 +7,8 @@ function fishInfoInit(contextPath){
 
 function startCuration(){
     getQuestionList(function(data){
-        getListDetail(count, data, function(listData){
-            drawCuration(listData);
+        getListDetail(data, function(listData){
+            drawCuration(count, listData);
         });
     });
 
@@ -32,58 +30,80 @@ function getQuestionList(callback){
     })
 }
 
-function getListDetail(count, data,callback){
+function getListDetail(data,callback){
     
-        let Qtitle = "";
-        let Q = "";
+    let Qtitle1 = "", Qtitle2 = "", Qtitle3 = "", Qtitle4 = "";
+    let Q1 = "", Q2 = "", Q3 = "", Q4 = "";
+
         for(let a = 0 ; a < data.length; a++){
             if(data[a].bigQ == "키우고싶은 종류가 있나요 ?"){
-                Qtitle = data[a].bigQ;
-                Q += `
-                        <p class="smallQ" onclick="getListDetail"> ${data[a].smallQ} </p>    
-                    `
+                Qtitle1 = data[a].bigQ;
+                Q1 += `
+                        <p class="smallQ" onclick="drawCuration(count + 1, listData);"> ${data[a].smallQ} </p>    
+                    `;
             } else if(data[a].bigQ == "성체가 됐을 때 어느정도 크기였으면 좋겠나요?"){
-                Qtitle = data[a].bigQ;
-                Q += `
-                        <p class="smallQ" onclick="getListDetail"> ${data[a].smallQ} </p>    
-                    `
+                Qtitle2 = data[a].bigQ;
+                Q2 += `
+                        <p class="smallQ" onclick="drawCuration(count + 1, listData);"> ${data[a].smallQ} </p>    
+                    `;
             } else if(data[a].bigQ == "집에 있는 시간이 어느정도 되나요 ?"){
-                Qtitle = data[a].bigQ;
-                Q += `
-                         <p class="smallQ" onclick="getListDetail"> ${data[a].smallQ} </p>    
-                    `
+                Qtitle3 = data[a].bigQ;
+                Q3 += `
+                         <p class="smallQ" onclick="drawCuration(count + 1, listData);"> ${data[a].smallQ} </p>    
+                    `;
             } else {
-                Qtitle = data[a].bigQ;
-                Q += `
-                         <p class="smallQ" onclick="getListDetail"> ${data[a].smallQ} </p>    
-                    `
+                Qtitle4 = data[a].bigQ;
+                Q4 += `
+                         <p class="smallQ" onclick="drawCuration(count + 1, listData);"> ${data[a].smallQ} </p>    
+                    `;
             }
         }
-    callback(Qtitle, Q);
+
+        
+        callback([
+            [Qtitle1, Q1],
+            [Qtitle2, Q2],
+            [Qtitle3, Q3],
+            [Qtitle4, Q4]
+        ]);
 }
 
 
-function drawCuration(listData){
+function drawCuration(count, listData){
     const url = path + `/resources/image/whale.png`
     let str = "";
     let Section = document.getElementById("main-div");
+    console.log(count)
+    console.log(listData[count])
+    if(count==4){
+        resultAjax();
+    }
 
     str+= `
             <div id="cu-explain-change">
-                <p>${data[0].bigQ}</p>
+                <p>${listData[count][0]}</p>
             </div>
             <div id="cu-question-div">
                 <div id="cu-img-change">
                     <img src="${url}" alt="">
                 </div>
                 <div id="cu-sub-explain-change">
-                    <p>${data[0].smallQ}</p>
+                    ${listData[count][1]}
                 </div>
             </div>
-            <div id="cu-btn-change">
-                <button onclick="startCuration();">시작하기</button>
-            </div>
-        `
+        `;
+
+    Section.innerHTML = str;
+}
+
+function resultAjax(){
+    const url = path + `/resources/image/whale.png`
+    let str = "";
+    let Section = document.getElementById("main-div");
+
+    str+= `
+            ㄲㅡㅌ
+        `;
 
     Section.innerHTML = str;
 }
