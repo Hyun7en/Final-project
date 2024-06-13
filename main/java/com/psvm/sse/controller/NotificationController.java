6739191f562f6c7500c6b1c77cd2d9aba6824c7d
DodleD <kdh030777@gmail.com>
2024-06-13 17:39:30 +0900

    [김도헌] 알람기능 개발 거의완

--- main/java/com/psvm/sse/controller/NotificationController.java
+++ main/java/com/psvm/sse/controller/NotificationController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -12,17 +13,19 @@ import com.psvm.sse.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping(value = "notifications/")
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
 
-    @GetMapping(value = "subscribe",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "subscribe.pr/{userNo}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@PathVariable Long userNo) {
+
         return notificationService.subscribe(userNo);
     }
 
-    @PostMapping("send-data")
+    @PostMapping("send-data.pr/{userNo}")
     public void sendData(@PathVariable Long userNo) {
-        notificationService.notify(userNo, "data");
+        notificationService.notify(userNo, "상품이 추가되었습니다.");
     }
 }
